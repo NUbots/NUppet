541422423f1b0f699d66245395f8c980b23a56ea
Brendan Annable <brendan.annable@uon.edu.au>
2014-04-29 15:47:42 +0100

    Initial Commit

---
+++ manifests/nodes.pp
@@ -0,0 +1,26 @@
+class initial_apt_update {
+  # Perform a single `apt-get update` before installing ANY packages.
+  exec { "initial_apt_update":
+    command => "/usr/bin/apt-get update"
+  } -> Package <| |>
+}
+
+node /^darwin\d+$/ {
+  include initial_apt_update
+
+  # # define variables for this node
+  $username = 'darwin'
+
+  # essential dependencies
+  package { 'libzmq3': ensure => latest, }
+  package { 'libarmadillo4': ensure => latest, }
+  package { 'libprotobuf8': ensure => latest, }
+
+  # Non-essential developer tools:
+  class { 'vim':  username => $username, }
+  package { 'screen': ensure => latest, }
+  package { 'htop': ensure => latest, }
+  package { 'gdb': ensure => latest, }
+  package { 'linux-headers-generic': ensure => latest, }
+  package { 'dos2unix': ensure => latest, }
+}
