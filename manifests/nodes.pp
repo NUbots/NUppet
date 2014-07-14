8ed767a63d6aab0be81690942f6bace537c13cc2
Brendan Annable <brendan.annable@uon.edu.au>
2014-07-14 20:22:08 +1000

    Added libyaml to dependencies. Now explicitly installs wpasupplicant.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -12,18 +12,19 @@ node /^darwin\d+$/ {
   $username = 'darwin'
 
   # essential dependencies
-  package { 'libzmq3': ensure => latest, }
-  package { 'libarmadillo4': ensure => latest, }
-  package { 'libtcmalloc-minimal4': ensure => latest, }
-  package { 'libprotobuf8': ensure => latest, }
+  package { 'libzmq3': ensure => latest }
+  package { 'libarmadillo4': ensure => latest }
+  package { 'libtcmalloc-minimal4': ensure => latest }
+  package { 'libprotobuf8': ensure => latest }
+  package { 'libyaml-cpp0.5': ensure => latest }
   package { 'libjpeg-turbo8': ensure => latest }
   package { 'software-properties-common': ensure => latest }
 
   # Non-essential developer tools:
-  class { 'vim':  username => $username, }
-  package { 'screen': ensure => latest, }
-  package { 'htop': ensure => latest, }
-  package { 'gdb': ensure => latest, }
-  package { 'linux-headers-generic': ensure => latest, }
-  package { 'dos2unix': ensure => latest, }
+  class { 'vim':  username => $username }
+  package { 'screen': ensure => latest }
+  package { 'htop': ensure => latest }
+  package { 'gdb': ensure => latest }
+  package { 'linux-headers-generic': ensure => latest }
+  package { 'dos2unix': ensure => latest }
 }
